6089f6aabc4e9063cd6012f9fbf7d13ca27afb79
dgrant59 <danielgrant422@gmail.com>
2022-08-22 00:45:41 -0400

    Cleaned up files, added comments, added example maps

--- TorontoLanguageMap.R
+++ TorontoLanguageMap.R
@@ -1,55 +1,101 @@
 library(tidyverse)
 library(sf)
-library(readxl)
 library(data.table)
 library(ggspatial)
 library(magrittr)
-library(fuzzyjoin)
-library(tmap)
-library(RColorBrewer)
 library(svglite)
-library(ggnewscale)
-# setwd("PATHHERE")
 
+#Popdata is statcan Census data at the dissemination area (DA) level. Found here
+#https://www12.statcan.gc.ca/census-recensement/2021/dp-pd/prof/details/download-telecharger.cfm?Lang=E
 
-colnames_popdata <-  colnames(fread("98-401-X2021006_English_CSV_data_Ontario.csv",encoding = "Latin-1",skip=0,nrows=1))
-popdata <- fread("98-401-X2021006_English_CSV_data_Ontario.csv",encoding = "Latin-1",skip=7593826,nrows=5192928)
+#Canada's shapemap files are available here
+#https://www12.statcan.gc.ca/census-recensement/2021/geo/sip-pis/boundary-limites/index2021-eng.cfm?year=21
+#where you can choose DA, census division, or census subdivision levels to correspond
+#to the census data you want.
+
+#Read in column names, add them to data taken after skipping 7593826 lines 
+#(this is where Toronto starts in the Canada-wide census data)
+#refer to StatCan's supplemental file that indicates where locations are within 
+#the large main file
+
+colnames_popdata <-  colnames(fread("98-401-X2021006_English_CSV_data_Ontario.csv",
+                                    encoding = "Latin-1",
+                                    skip=0,
+                                    nrows=1))
+popdata <- fread("98-401-X2021006_English_CSV_data_Ontario.csv",
+                 encoding = "Latin-1",
+                 skip=7593826,
+                 nrows=5192928)
 colnames(popdata) <- colnames_popdata
 
-popdata <- popdata[,c(1:5,8:12,18)]
+#Take only a subset of columns, year, location data, counts, %s
+head(popdata)
+popdata %<>% popdata[,c(1:5,8:12,18)]
+
+#Since data is hierarchical, only take DA-level data
+#(Can switch this for "Census division" or "Census subdivision" if you want
+#to plot less granularly)
 popdata %<>% filter(GEO_LEVEL=="Dissemination area") 
 
-popdata %<>% filter(CHARACTERISTIC_ID%in%c(725:1045))#725 for french
+# Each DA has >1000 attributes recorded. We only want attributes associated
+# with "Most common language spoken at home", i.e. CHARACTERISTIC_ID 725-1045
+# to include English, change to 724, to exclude both Eng and French, change to 726
+
+popdata %<>% filter(CHARACTERISTIC_ID%in%c(725:1045))
+
+#Language data here is also hierarchical, i.e. 
+# Non-Indigenous languages
+#   Afro-Asiatic languages
+#     Semitic languages
+#       Arabic
+# and statistics are summarized at each level
+
+# I am only interested in the individual languages.
+# I discovered that individual languages never contain the word "languages"
+# BUT I was still interested in categories of the form "German languages, n.X.X"
+# filter all "not otherwise indicated" languages out, remove all 
+# characteristics with the word "languages", remove all other "n.X.X" languages
+# then re add the "not otherwise indicated" languages.
+
 popdatanie <- filter(popdata, grepl("n\\.",CHARACTERISTIC_NAME))
 popdata <- filter(popdata, !grepl("languages",CHARACTERISTIC_NAME))
 popdata <- filter(popdata, !grepl("n\\.",CHARACTERISTIC_NAME))
 popdata <- rbind(popdata, popdatanie)
 popdata$CHARACTERISTIC_NAME <- trimws(popdata$CHARACTERISTIC_NAME,"both")
 
-# data_qual <- popdata[,c(3,6)] %>% group_by(ALT_GEO_CODE) %>% summarise(Data_qual = toString(DATA_QUALITY_FLAG))
-# 
-# popdata <- popdata[,-c(6,7,9,11)] %>% group_by(ALT_GEO_CODE) %>% pivot_wider(names_from=CHARACTERISTIC_NAME,values_from=C1_COUNT_TOTAL)
-# popdata <- left_join(popdata,data_qual,by=c("ALT_GEO_CODE"="ALT_GEO_CODE"))
-# popdata %<>% mutate(ALT_GEO_CODE=factor(ALT_GEO_CODE))
 
+#List of unique languages that we have data on
 languages <- unique(popdata$CHARACTERISTIC_NAME)
 
-for(i in 1:1){
+#Looping through all languages to create .svg files for each
+
+for(i in 1:length(languages)){
   print(languages[i])
   ###### MAP DATA
   canada <- read_sf(dsn = "./ShapeFiles/DA Shapefiles/DA Map.shp", 
                     stringsAsFactors = T)
   
   #Join map data to case data for easy ggplotting
-  canada <- left_join(canada,popdata %>% group_by(ALT_GEO_CODE) %>% filter(CHARACTERISTIC_NAME==languages[i]), by=c("DGUID"="DGUID")) 
+  #Each DA (or CD/CS) has a specific ID called DGUID, match this to the ones found
+  #in the .shp file
+  canada <- left_join(canada,popdata %>% 
+                        group_by(ALT_GEO_CODE) %>% 
+                          filter(CHARACTERISTIC_NAME==languages[i]), 
+                                 by=c("DGUID"="DGUID")) 
+  
+  #"canada" is all of canada, we only want Toronto so only use DGUID from popdata
   temp <- canada[canada$DGUID%in%popdata$DGUID,]
+  
+  #if nobody in toronto speaks that language (or too few to be shown at 1 decimal
+  #place), skip mapping it
   if(sum(temp[!is.na(temp$C10_RATE_TOTAL),]$C10_RATE_TOTAL!=0)){
     ggplot()+
-      annotation_spatial(temp,lwd=0.05)+
-      layer_spatial(temp,aes(fill=C10_RATE_TOTAL),color="grey91",lwd=0.03)+
-      scale_fill_gradient(low = "white", high = "darkgreen",na.value = "red",
+      layer_spatial(temp,aes(fill=C10_RATE_TOTAL,color=""),lwd=0.05)+
+      scale_fill_gradient(low = "white", high = "blue",na.value = "grey20",
                           breaks=c(0,max(temp[!is.na(temp$C10_RATE_TOTAL),]$C10_RATE_TOTAL)),
                           guide = guide_colorbar(frame.colour = "black", ticks.colour = "black"))+
+      scale_colour_manual(values="black") +              
+      guides(colour=guide_legend("No data", override.aes=list(fill="grey20")))+
       theme(legend.title.align=0,
             axis.line=element_blank(),  #bunch of options to remove "graph" visuals
             axis.text.x=element_blank(),
@@ -61,16 +107,27 @@ for(i in 1:1){
             panel.border=element_blank(),
             panel.grid.major=element_blank(),
             panel.grid.minor=element_blank(),
-            plot.title = element_text(hjust = 0.5,vjust=-10,size=18,face="bold"),
+            plot.title = element_text(hjust = 0.5,
+                                      vjust=-15,
+                                      size=22,
+                                      face="bold",
+                                      margin = margin(-20,0,-20,0)),
             legend.text=element_text(size=10),  #legend text sizes
             legend.title=element_text(size=13),
             plot.margin = unit(c(-1, -1, -0.9, -1), "cm"),
             legend.box.margin=margin(0,0,0,-80))+ 
-     guides(fill= guide_colourbar(barwidth=1.5,barheight=6,title.position="top",title = "% Speaking \nLanguage"))+
+      guides(fill=guide_colourbar(barwidth=1.5,
+                                  barheight=6,
+                                  title.position="top",
+                                  title = "% Speaking \nLanguage"))+
       ggtitle(paste0("% Speaking\n",languages[i]," \n Most Often at Home \n (2021)"))
-    ggsave(paste0("Language",i,".svg"),path="./TorontoMaps/")
+    
+    #Save to .svg
+    # ggsave(paste0(sub("\\,.*", "", languages[i]),".svg"),path="./TorontoMaps/",
+    #        width=3500,
+    #        height=3000,
+    #        dpi=300,
+    #        units="px")
   }
-  
-  
 }
 
